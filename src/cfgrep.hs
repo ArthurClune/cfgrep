e51b930d3f3bbfb56a5f45de6d12f129707a8c4d
Arthur Clune <arthur@clune.org>
2013-07-04 20:14:14 +0100

    Make program act like a proper Unix citizen
    Now takes input on stdin or multiple files on the command line

--- src/cfgrep.hs
+++ src/cfgrep.hs
@@ -1,17 +1,19 @@
 {-# Language OverloadedStrings #-}
 
-import           Data.Monoid                (mempty)
-import           Data.List.Split
-import           Options.Applicative
-import           Text.HandsomeSoup          (css)
-import           Text.XML.HXT.Core hiding   (trace)
-import           Text.XML.HXT.TagSoup
+import Control.Monad              ((>=>))
+import Data.Monoid                (mempty)
+import Data.List.Split            (split, keepDelimsL, onSublist)
+import Options.Applicative        ((<*>), (<$>), argument, arguments,
+                                   execParser, idm, info, metavar, str)
+import System.IO                  (Handle, IOMode(..), hGetContents, stdin, openFile)
+import Text.HandsomeSoup          (css)
+import Text.XML.HXT.Core hiding   (trace)
+import Text.XML.HXT.TagSoup
 
 data CFGrep = CFGrep {
-                     pattern  :: String,
-                     filename :: String
+                     pattern :: String,
+                     files :: [String]
                      } deriving (Show)
-
 
 findMatches::String -> String -> IO [String]
 findMatches pat page = do
@@ -21,12 +23,25 @@ findMatches pat page = do
                     page >>> css pat
     return $ split (keepDelimsL $ onSublist ('<' : pat)) (head results)
 
+matchOverHandle::String->Handle->IO [String]
+matchOverHandle pat = hGetContents >=> findMatches pat
+
+printWithFilename::(String, [String]) -> IO ()
+printWithFilename (name, results) = mapM_ (putStrLn . (\x -> name ++ ": " ++ x)) results
+
 main :: IO ()
 main = do
     options <- execParser opts
-    matches <- readFile (filename options) >>= findMatches (pattern options)
-    mapM_ putStrLn matches
+    case files options of
+         []  -> matchOverHandle (pattern options) stdin
+                >>= mapM_ putStrLn
+         [f] -> openFile f ReadMode
+                >>= matchOverHandle (pattern options)
+                >>= mapM_ putStrLn
+         l   -> mapM (\f -> openFile f ReadMode
+                >>= matchOverHandle (pattern options)) l
+                >>= \x -> mapM_ printWithFilename (zip (files options) x)
   where
-    parser = CFGrep <$> argument str (metavar "pattern")
-                    <*> argument str (metavar "filename")
+    parser = CFGrep <$> argument  str (metavar "pattern")
+                    <*> arguments str (metavar "filenames")
     opts = info parser mempty
